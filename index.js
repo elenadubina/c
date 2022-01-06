92879630b20f0df7d2d2c9eadd985314446c8325
Elena Dubina <elena.dubina89@gmail.com>
2022-01-06 16:24:49 +0200

    modified list of techs and some info

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // This is an alert when smb hovers my email form 
-document.getElementById('form').addEventListener('mouseover', function () {
-    alert("This form is for demonstration only. Contact me on email or Linked!");
+document.getElementById('button').addEventListener('click', function () {
+    alert("This form is for demonstration only. Contact me via email or Linked!");
 });
